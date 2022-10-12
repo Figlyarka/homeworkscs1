3fb4007292eda370fd808687a36a16507207db2e
figlyarka <yaiyaksusha@mail.ru>
2022-10-13 00:01:08 +0500

    task3 done

---
+++ numbers task3/numbers task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbers_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число с колвом десятков неравным 0");
+            int num = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine(num % 10 * 100 + num % 100 - num % 10 + num / 100);
+            Console.ReadKey();
+        }
+    }
+}
