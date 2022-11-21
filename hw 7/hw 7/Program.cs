7f7e45257e564cd155bcd1828275b2abe09c7875
figlyarka <yaiyaksusha@mail.ru>
2022-11-21 14:58:56 +0500

    kddklmpmpvp

---
+++ hw 7/hw 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine(k);
+
+        }
+    }
+}
