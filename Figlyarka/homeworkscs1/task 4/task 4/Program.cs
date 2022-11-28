124d2047a13ef4fcdc066ff0a0c86e71f98ee0ab
figlyarka <yaiyaksusha@mail.ru>
2022-11-28 15:02:44 +0500

    дз 5 сделано

---
+++ Figlyarka/homeworkscs1/task 4/task 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Abs(x + (Math.Abs(x + Math.Abs(x + Math.Sqrt(x + 1))))));
+            Console.ReadKey();
+        }
+    }
+}
