949857df368da7687e881fd677d93bc06070c6ac
figlyarka <yaiyaksusha@mail.ru>
2022-12-05 00:12:38 +0500

    7.2 done

---
+++ 7.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите х и у");
+            double x=double.Parse(Console.ReadLine());
+            double y=double.Parse(Console.ReadLine());
+            Console.WriteLine(Isdotinarea(x,y));
+            
+        }
+        static bool Isdotinarea(double x,double y)
+        {
+            if ((y >= -2) && ((x <= -3) || (x >= -1)))
+            {
+                return true;
+            }
+            return false;   
+        }
+    }
+}
