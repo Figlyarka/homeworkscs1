1ac5c24428d2734d6965f0460f5054f6f01b495c
figlyarka <yaiyaksusha@mail.ru>
2022-12-04 19:31:17 +0500

    hw8 done

---
+++ 8дз/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw8
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine(Func(x));
+            Console.ReadKey();
+        }
+        static double Func(double x)
+        {
+            if (x > 0)
+                return Math.Log(x);
+            else if (x <= 0)
+                return 0;
+            else
+                return -Math.Log(x);
+
+
+
+        }
+    }
+}
