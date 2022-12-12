fc044a058c7a3ad15b78a9bc04e9bd4021123e58
figlyarka <yaiyaksusha@mail.ru>
2022-12-12 21:56:37 +0500

    9/2 done

---
+++ 92 well done/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число ");
+            double n = double.Parse(Console.ReadLine());
+            Console.ReadKey();
+            double a = 1;
+            while (a <= n)
+            {
+                double square = Math.Pow(a, 2);
+                Console.WriteLine($"квадрат числа: {a} равен: {square} ");
+                a++;
+            }
+
+
+
+        }
+    }
+}
