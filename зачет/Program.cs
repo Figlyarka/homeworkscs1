c5ed88ab7eb3e8f97ade1f477b820c4c69c9c546
figlyarka <yaiyaksusha@mail.ru>
2022-12-27 15:16:08 +0500

    ввукупкум

--- зачет/Program.cs
+++ зачет/Program.cs
@@ -10,54 +10,120 @@ namespace зачет
     {
         static void Main()
         {
-            List<char> vowels = new List<char> { 'а', 'о', 'ы', 'и', 'у', 'э', 'ё', 'я', 'е', 'ю', 'А', 'О', 'Ы', 'И', 'У', 'Э', 'Ё', 'Я', 'Е', 'Ю' };
-            Console.WriteLine("введите строку на русском языке");
-            string str = Console.ReadLine();
-            int all = 0;
-            if (IsRightStr(str))
-            {
-                for (int i = 0; i < vowels.Count/2; i++)
-                {
-                    int count = 0;
-                    if (str.Contains(vowels[i]))
-                    {
-                        count = str.Count(s => s.Equals(vowels[i]));
-                        count += str.Count(s => s.Equals(vowels[i+10]));
-                        Console.WriteLine($"Буква {vowels[i]} вcтречается {count} раз");
-                        all += count;
+            
 
+            Console.WriteLine("Введите строку на русском языке");
+            string sentance = Console.ReadLine();
+            if (AreAllLetters(sentance))
+            {
+                string phrase2 = sentance;
+                phrase2 = phrase2.Replace("а", "1");
+                phrase2 = phrase2.Replace("о", "2");
+                phrase2 = phrase2.Replace("у", "3");
+                phrase2 = phrase2.Replace("е", "4");
+                phrase2 = phrase2.Replace("ы", "5");
+                phrase2 = phrase2.Replace("ё", "6");
+                phrase2 = phrase2.Replace("э", "7");
+                phrase2 = phrase2.Replace("ю", "8");
+                phrase2 = phrase2.Replace("я", "9");
+                phrase2 = phrase2.Replace("и", "0");
+                Console.WriteLine(phrase2);
+                int a = 0;
+                int o = 0;
+                int u = 0;
+                int e = 0;
+                int p = 0;
+                int yo = 0;
+                int ee = 0;
+                int yu = 0;
+                int ya = 0;
+                int ii = 0;
+                for (int i=0; i<phrase2.Length;i++)
+                {
+                    if (phrase2[i]==Convert.ToChar("1"))
+                    {
+                        a++;
+                    }
+                    if (phrase2[i]== Convert.ToChar("2"))
+                    {
+                        o++;
+                    }
+                    if (phrase2[i]== Convert.ToChar("3"))
+                    {
+                        u++;
+                    }
+                    if (phrase2[i]== Convert.ToChar("4"))
+                    {
+                        e++;
+                    }
+                    if (phrase2[i] == Convert.ToChar("5"))
+                    {
+                        p += 1;
+                    }
+                    if (phrase2[i] == Convert.ToChar("6"))
+                    {
+                        yo += 1;
+                    }
+                    if (phrase2[i] == Convert.ToChar("7"))
+                    {
+                        ee++;
+                    }
+                    if (phrase2[i] == Convert.ToChar("8"))
+                    {
+                        yu++;
+                    }
+                    if (phrase2[i] == Convert.ToChar("9"))
+                    {
+                        ya++;
+                    }
+                    if (phrase2[i] == Convert.ToChar("0"))
+                    {
+                        ii++;
                     }
 
                 }
+                Console.WriteLine(a + "колво а");
+                Console.WriteLine(o + "колво о");
+                Console.WriteLine(u + "колво у");
+                Console.WriteLine(e + "колво е");
+                Console.WriteLine(p + "колво ы");
+                Console.WriteLine(yo + "колво ё");
+                Console.WriteLine(ee + "колво э");
+                Console.WriteLine(yo + "колво ё");
+                Console.WriteLine(yu + "колво ю");
+                Console.WriteLine(ya + "колво я");
 
-                Console.WriteLine(all + " общее количество гласных");
+                int allvowels = a + o + u + e + p + yo + ee + yo + yu + ya;
+                Console.WriteLine( allvowels + " общее количество гласных");
             }
             else
             {
-                Console.WriteLine("");
+               Console.WriteLine("неверный формат");
             }
             
 
 
             Console.ReadKey();
         }
-        static bool IsRightStr(string str)
+
+        static bool AreAllLetters(string sentance)
         {
             int count = 0;
-            foreach (char c in str)
+            foreach (char c in sentance)
             {
-                if (Char.IsLetter(c) || Char.IsPunctuation(c))
+                if (Char.IsLetter(c))
                 {
                     count++;
                 }
+
             }
-            if (count==str.Length)
+            if (count < sentance.Length)
             {
-                return true;
+                return false;
             }
             else
             {
-                return false;
+                return true;
             }
         }
     }
