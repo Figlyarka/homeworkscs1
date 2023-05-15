e56b60c57ec242b7067f75521c86a8439ffa3724
figlyarka <yaiyaksusha@mail.ru>
2023-05-15 15:35:13 +0500

    hw 13

---
+++ ArmyLibrary.UnitTests/TypeOfService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ArmyLibrary
+{
+    public enum TypeOfService{Contract,Urgent}
+}
